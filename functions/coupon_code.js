28d6f9f9017b1167fc006204a42d263d681bf182
Avinash <avinash@avinash.com.np>
2019-06-11 11:47:09 +0545

    add buy page

---
+++ functions/coupon_code.js
@@ -0,0 +1,28 @@
+var CouponCodes = {
+  "WWFEMALE" : {
+    "npr": "2000",
+    "usd": "30"
+  },
+}
+
+exports.handler = function(event, context, callback) {
+  var rate = {
+    "npr" : "3500",
+    "usd" : "50"
+  }
+
+  try {
+    var body = JSON.parse(event.body)
+
+    if(body.coupon_code && CouponCodes[body.coupon_code]) {
+      rate = CouponCodes[body.coupon_code]
+    }
+  } catch (e) {
+    // ignore that shit
+  }
+
+  callback(null,{
+    statusCode:200,
+    body: JSON.stringify(rate)
+  });
+}
